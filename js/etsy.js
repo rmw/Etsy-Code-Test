8b63639e26896dc4c7f6bf634782d681bd56a3eb
Rebecca Miller-Webster <rebecca@miller-webster.com>
2012-03-28 12:53:39 -0400

    attempt at using jquery visual for price to favorites comparison

--- js/etsy.js
+++ js/etsy.js
@@ -18,10 +18,34 @@ var ETSY = ETSY || {}; (function($) {
 							console.log(data);
 							//multiple by -1 to reverse the sort (most favorited first)
 							var sorted = _.sortBy(data.results, function(d) { return -1 * d.num_favorers; });
+							var price_max = 0;
+							//set to high val - not the best way
+							var price_min = 10000000000000000000000000000;
+							var prices = {};
 							var results = _.map(sorted, function(d) {
+								price_max = Math.max(price_max, d.price);
+								price_min = Math.min(price_min, d.price);
+								prices[d.price.toString()] = (prices[d.price.toString()] || 0) + d.num_favorers;
 								return d.title + ': ' + d.num_favorers + " " + d.price;
 							});
 							$('<p>' + results.join('<br />') + '</p>').appendTo('#etsy-images');
+							$('<p>' + "Price range: $" + price_max + " - $" + price_min + "</p>").prependTo('#etsy-images');
+							//create chart table
+							var price_keys = _.sortBy(_.keys(prices), function(p) { return parseFloat(p); });
+							console.log(price_keys);
+							var table = "<th scope=\"row\">" + shop_id + "</th>";
+							//header
+							var table_header = "<thead><tr><td></td>";
+							for(var i=0; i < price_keys.length; ++i) {
+								var key = price_keys[i]; 
+								table_header += "<th scope=\"col\">" + key + "</th>"
+								table += "<td>" + prices[key] + "</td>";
+							}
+							table_header += "</tr></thead><tbody><tr>";
+							table = "<table><caption>Price Distribution</caption>" + table_header + table;
+							table += "</tr></tbody></table>"
+							$(table).appendTo('#etsy-images');
+							$('table').visualize({type: 'line', width: '420px'});
 						} else {
 							$('<p>No results.</p>').appendTo('#etsy-images');
 						}
